e099b3107e0960afee406d6cfd502b3efb0e6763
IanV <ianverlinden1.iv@gmail.com>
2022-11-06 21:49:43 +0100

    Add project files.

---
+++ ViewComponents/ViewComponents/Reclame.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace ViewComponents.ViewComponents
+{
+    public class Reclame : ViewComponent
+    {
+        static string[] powerbars = { "mars", "bounty", "snickers", "twix" };
+        public IViewComponentResult Invoke(string tekst, bool html)
+        {
+            if (html)
+                return new HtmlContentViewComponentResult(new HtmlString(tekst));
+            else
+                return Content(tekst);
+
+            /*return new HtmlContentViewComponentResult(new HtmlString("Deze tekst zal <letterlijk> worden weergegeven."));*/
+
+            /*return Content("Deze tekst zal <letterlijk> worden weergegeven.");*/
+
+            /*Random random = new();
+            int randomNummer = random.Next(0, 4);
+            return View((object)powerbars[randomNummer]);*/
+        }
+    }
+}
